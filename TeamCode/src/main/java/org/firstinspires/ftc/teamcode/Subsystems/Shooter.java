f042021f13d04f332e51a248be69aa0eeaf47796
Jaydenwithawhy <jaydenuvbiama@gmail.com>
2026-09-20 15:41:25 -0600

    shooter testing time :D

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.aaravlabs.synapse.Node;
+import com.aaravlabs.synapse.Orchestrator;
+
+public class Shooter extends Node {
+    protected Shooter(Orchestrator orchestrator) {
+        super(orchestrator);
+    }
+}
