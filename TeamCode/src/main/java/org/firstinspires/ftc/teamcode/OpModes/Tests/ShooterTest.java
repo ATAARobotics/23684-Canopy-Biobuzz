8b6b902fe4997c5b99484e4314e53f552efa68ab
Jaydenwithawhy <jaydenuvbiama@gmail.com>
2026-09-20 16:08:56 -0600

    shooter testing time :D

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/ShooterTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.OpModes.Tests;
+
+import com.aaravlabs.synapse.ftc.SafeDevice;
+import com.aaravlabs.synapse.ftc.SafeOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
+
+public class ShooterTest extends SafeOpMode {
+
+    private SafeDevice<DcMotorEx> shooter;
+
+    public static int RPM;
+
+    @Override
+    protected void onSafeInit() {
+        shooter = safeMap.device(DcMotorEx.class, "shooter");
+
+        orch.registerNode("shooter", new Shooter(orch,shooter));
+    }
+
+    @Override
+    protected void onSafeLoop() {
+
+        telemetry.addData("shooterSpeed",RPM);
+    }
+}
