85d504b15920e05a856ba59bf898b9a13bf68ef7
Jaydenwithawhy <jaydenuvbiama@gmail.com>
2026-09-25 19:07:57 -0600

    SAFEPEDRO!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,11 +1,75 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.aaravlabs.safepedropathing.algorithm.Foresight;
+import com.aaravlabs.safepedropathing.algorithm.ForesightConfig;
+import com.aaravlabs.safepedropathing.controllers.Controller;
 import com.aaravlabs.safepedropathing.follower.Follower;
+import com.aaravlabs.safepedropathing.math.Matrix;
+import com.aaravlabs.safepedropathing.math.Vector2D;
+import com.aaravlabs.safepedropathing.revhub.drivetrains.Mecanum;
+import com.aaravlabs.safepedropathing.revhub.drivetrains.MecanumConfig;
+import com.aaravlabs.safepedropathing.revhub.localizers.PinpointConfig;
+import com.aaravlabs.safepedropathing.revhub.localizers.PinpointLocalizer;
 import com.aaravlabs.synapse.ftc.SafeHardwareMap;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
-    public static Follower create(SafeHardwareMap safeMap) {
-        // return new Follower(localizer, drivetrain, algorithm);
-        return null;
-    }
+        public static MecanumConfig drivetrainConfig = new MecanumConfig(c -> {
+            c.frontLeftName.set(" frontLeft");
+            c.frontRightName.set("frontRight");
+            c.backLeftName.set("backLeft");
+            c.backRightName.set("backRight");
+            c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+            c.frontRightDirection.set(DcMotorSimple.Direction.FORWARD);
+            c.backLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+            c.backRightDirection.set(DcMotorSimple.Direction.FORWARD);
+        });
+
+        public static PinpointConfig localizerConfig = new PinpointConfig(c -> {
+            c.name.set("odoComp");
+            c.podType.set(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+            c.xPodOffset.set(4.716052258108545);
+            c.yPodOffset.set(-0.1736801628052719);
+            c.xPodDirection.set(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+            c.yPodDirection.set(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+            c.globalDistanceUnit.set(DistanceUnit.INCH);
+            c.offsetUnits.set(DistanceUnit.INCH);
+        });
+
+        public static ForesightConfig foresightConfig = new ForesightConfig(
+                c -> {
+                    Controller primaryTranslationalForward = Controller.proportional(0.2202719446529322);
+                    Controller secondaryTranslationalForward = Controller.proportional(0.08138459777449746);
+                    Controller primaryTranslationalLateral = Controller.proportional(0.31329175178980395);
+                    Controller secondaryTranslationalLateral = Controller.proportional(0.1157529309765481);
+
+                    c.forwardTranslational.set(Controller.piecewise(secondaryTranslationalForward).put(2.5, primaryTranslationalForward));
+                    c.strafeTranslational.set(Controller.piecewise(secondaryTranslationalLateral).put(2.5, primaryTranslationalLateral));
+
+                    c.coast.set(Controller.proportionalFeedforward(0.0095690587013686));
+                    c.brake.set(Controller.proportionalFeedforward(0.00813369989616331));
+
+                    c.headingFeedback.set(Controller.proportional(4.650083943643753));
+                    c.headingBrakeCoefficients.set(Vector2D.cartesian(0.17881558802022274, -0.005374054406345219));
+
+                    c.linearBrakeCoefficients.set(Matrix.diag(0.09666738236094859, 0.19512737059580584));
+                    c.quadraticBrakeCoefficients.set(Matrix.diag(0.0012397788720814437, -5.445777182152711E-4));
+
+                    c.maxAchievableForwardVelocity.set(91.56943771038799);
+                    c.maxAchievableStrafeVelocity.set(78.78967311721264);
+                    c.naturalForwardDeceleration.set(35.98607472684754);
+                    c.naturalStrafeDeceleration.set(57.251377442709554);
+                }
+        );
+        public static Follower create(SafeHardwareMap h) {
+            return new Follower(
+                    new PinpointLocalizer(h, localizerConfig),
+                    new Mecanum(h, drivetrainConfig),
+                    new Foresight(foresightConfig)
+            );
+        }
 }
